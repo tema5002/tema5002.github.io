ca2504de5317c9c72205ca62a5c33cfc1dd71424
tema5002 <152618947+tema5002@users.noreply.github.com>
2024-05-27 13:18:03 +0300

    Delete emojis.py

--- emojis.py
+++
@@ -1,5 +0,0 @@
-import temalib, os
-
-with temalib.editfile("emojis_.html") as f:
-    for i in os.listdir(temalib.get_folder_path(__file__, "emojis")):
-        f.write(f'        <div class="emoji"><img src="emojis/{i}" alt="{i}"><p>{i[:-4]}</p></div>\n')
